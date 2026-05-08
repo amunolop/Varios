cd2fd1365cb302e92eafec7935ab45804597d855
amunolop <155436849+amunolop@users.noreply.github.com>
2026-05-08 03:30:02 -0700

    Add files via upload

---
+++ python3-app-p2/python3-app-p2/src/main.py
@@ -0,0 +1,9 @@
+from utils import get_random_number
+
+def main():
+    print("Welcome to the Python 3 App!")
+    max_number = 10  # You can set this to any integer greater than 1
+    print(f"Random number: {get_random_number(max_number)}")
+
+if __name__ == "__main__":
+    main()
